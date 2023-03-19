ca755bb20c2a04ff8d2664b1cb6139dda7737e95
this-is-spear <rjsckdd12@gmail.com>
2023-03-19 10:29:01 +0900

    Test(Folder) 작성 인수테스트
    ###Description###
    - 작성(FolderAcceptanceTest) 인수테스트
    - 수정(AcceptanceTest) 인수테스트 리팩토링

---
+++ src/test/java/hello/numblemybox/integration/AcceptanceTemplate.java
@@ -0,0 +1,149 @@
+package hello.numblemybox.integration;
+
+import static hello.numblemybox.stubs.FileStubs.*;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.numblemybox.SpringBootTemplate;
+import hello.numblemybox.mybox.infra.MyBoxMongoRepository;
+
+class AcceptanceTemplate extends SpringBootTemplate {
+
+	@Autowired
+	protected ObjectMapper OBJECT_MAPPER;
+	@Autowired
+	protected WebTestClient webTestClient;
+	@Autowired
+	protected MyBoxMongoRepository myBoxRepository;
+
+	@BeforeEach
+	void setUp() throws IOException {
+		deleteFiles();
+		myBoxRepository.deleteAll().subscribe();
+	}
+
+	@AfterAll
+	static void afterAll() throws IOException {
+		deleteFiles();
+	}
+
+	private static void deleteFiles() throws IOException {
+		Files.deleteIfExists(프로덕션_업로드_사진_경로.resolve(그냥_문장));
+		Files.deleteIfExists(프로덕션_업로드_사진_경로.resolve(끝맺음_문장));
+		Files.deleteIfExists(프로덕션_업로드_사진_경로.resolve(인사_문장));
+	}
+
+	protected WebTestClient.BodyContentSpec 파일_다운로드_요청(String id) {
+		return webTestClient.post().uri("/mybox/{id}/download", id)
+			.contentType(MediaType.APPLICATION_OCTET_STREAM)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected WebTestClient.BodyContentSpec 파일_조회_요청(String filename) {
+		return webTestClient.get().uri("/mybox/files/{filename}", filename)
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected WebTestClient.BodyContentSpec 파일_조회_요청() {
+		return webTestClient.get().uri("/mybox/files")
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected WebTestClient.ResponseSpec 파일_업로드_요청(String... filenames) {
+		final var builder = new MultipartBodyBuilder();
+		final var requestPartName = "files";
+
+		for (int i = 0; i < filenames.length; i++) {
+			var filename = filenames[i];
+			builder.part("image", getFileOne(filename))
+				.header("Content-disposition",
+					String.format("form-data; name=\"%s\"; filename=\"%s\"", requestPartName, filename))
+				.contentType(MediaType.TEXT_PLAIN);
+		}
+
+		if (filenames.length == 0) {
+			builder.part("text", getFileOne(끝맺음_문장))
+				.header("Content-disposition",
+					String.format("form-data; name=\"%s\"; filename=\"%s\"", requestPartName, 끝맺음_문장))
+				.contentType(MediaType.TEXT_PLAIN);
+		}
+
+		return webTestClient.post().uri("/mybox/upload")
+			.contentType(MediaType.MULTIPART_FORM_DATA)
+			.bodyValue(builder.build())
+			.exchange()
+			.expectStatus().isOk();
+	}
+
+	protected WebTestClient.ResponseSpec 폴더_안_파일_업로드_요청(String foldername, String filename) {
+		final var builder = new MultipartBodyBuilder();
+		final var requestPartName = "files";
+		builder.part("text", getFileOne(filename))
+			.header("Content-disposition",
+				String.format("form-data; name=\"%s\"; filename=\"%s\"", requestPartName, 끝맺음_문장))
+			.contentType(MediaType.TEXT_PLAIN);
+		return webTestClient.post().uri("/mybox/folders/{foldername}/upload", foldername)
+			.contentType(MediaType.MULTIPART_FORM_DATA)
+			.bodyValue(builder.build())
+			.exchange()
+			.expectStatus().isOk();
+	}
+
+	protected WebTestClient.BodyContentSpec 폴더_생성_요청(String parentId, String foldername) {
+		return webTestClient.post()
+			.uri(uriBuilder -> uriBuilder.path("/mybox/folders/{parentId}")
+				.queryParam("foldername", foldername)
+				.build(parentId))
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected WebTestClient.BodyContentSpec 루트_폴더_조회_요청() {
+		return webTestClient.get()
+			.uri("/mybox/folders")
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected WebTestClient.BodyContentSpec 폴더_조회_요청(String folderId) {
+		return webTestClient.get()
+			.uri("/mybox/folders/{folderId}", folderId)
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+	}
+
+	protected String getString(byte[] responseBody) throws IOException {
+		var reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(responseBody)));
+		return reader.readLine();
+	}
+
+	protected <T> T getValue(byte[] data, Class<T> t) throws IOException {
+		return OBJECT_MAPPER.readValue(data, t);
+	}
+}
---
+++ src/test/java/hello/numblemybox/integration/FolderAcceptanceTest.java
@@ -0,0 +1,105 @@
+package hello.numblemybox.integration;
+
+import static hello.numblemybox.stubs.FileStubs.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Disabled
+class FolderAcceptanceTest extends AcceptanceTemplate {
+
+	/**
+	 * @Fact 사용자는 폴더 내용을 확인할 수 있다.
+	 * @Given 폴더를 생성한 후
+	 * @When 폴더를 조회하면
+	 * @Then 폴더 안 내용을 확인할 수 있다.
+	 */
+	@Test
+	void 폴더_내용을_확인한다() {
+		// given
+		var 첫_번째_루트_폴더_조회 = 루트_폴더_조회_요청();
+		var parentId = getRootId(첫_번째_루트_폴더_조회);
+		var foldername = "폴더_이름";
+
+		// when
+		폴더_생성_요청(parentId, foldername);
+
+		// then
+		var 두_번째_루트_폴더_조회 = 폴더_조회_요청(parentId);
+		assertThat(isContainsName(두_번째_루트_폴더_조회, foldername)).isTrue();
+	}
+
+	/**
+	 * @Fact 사용자는 폴더 안 내용에서 파일을 메타데이터를 조회한다.
+	 * @When 폴더 안에 파일이 있을 때
+	 * @Then 폴더 안 파일을 조회할 수 있다.
+	 */
+	@Test
+	void 루트_폴더_안_파일을_다운로드한다() throws IOException {
+		// when
+		파일_업로드_요청(그냥_문장);
+		var 루트_폴더_조회 = 루트_폴더_조회_요청();
+
+		// then
+		var fileId = getFileId(루트_폴더_조회);
+		var 파일_다운로드_요청 = 파일_다운로드_요청(fileId);
+		var responseBody = 파일_다운로드_요청.returnResult().getResponseBody();
+
+		assertNotNull(responseBody);
+
+		var 파일_내용 = getString(responseBody);
+
+		assertThat(파일_내용).isEqualTo(
+			getString(Files.readAllBytes(프로덕션_업로드_사진_경로.resolve(그냥_문장)))
+		);
+	}
+
+	/**
+	 * @Fact 사용자는 폴더를 생성하고 그 안에 파일을 업로드해서 관리할 수 있다.
+	 * @When 사용자는 폴더를 생성하면
+	 * @Then 그 안에 파일을 업로드해서 관리할 수 있다.
+	 */
+	@Test
+	void 폴더_안_파일을_다운로드한다() {
+		// given
+		var 첫_번째_루트_폴더_조회 = 루트_폴더_조회_요청();
+		var parentId = getRootId(첫_번째_루트_폴더_조회);
+		var foldername = "폴더_이름";
+
+		// when
+		폴더_생성_요청(parentId, foldername);
+		var 두_번째_루트_폴더_조회 = 폴더_조회_요청(parentId);
+		var folderId = getNameContainsFolderName(두_번째_루트_폴더_조회, foldername);
+
+		// then
+		폴더_안_파일_업로드_요청(folderId, 그냥_문장);
+		var 세_번째_루트_폴더_조회 = 폴더_조회_요청(parentId);
+		assertThat(isContainsName(세_번째_루트_폴더_조회, 그냥_문장)).isTrue();
+	}
+
+	private String getNameContainsFolderName(WebTestClient.BodyContentSpec spec, String foldername) {
+		// TODO 응답 데이터에서 파일 이름과 맞는 식별자 조회
+		return null;
+	}
+
+	private String getFileId(WebTestClient.BodyContentSpec spec) {
+		// TODO 응답 데이터에서 파일 식별자 조회
+		return null;
+	}
+
+	private String getRootId(WebTestClient.BodyContentSpec spec) {
+		// TODO 응답 데이터에서 현재 위치한 폴더 정보 반환
+		return null;
+	}
+
+	private boolean isContainsName(WebTestClient.BodyContentSpec spec, String foldername) {
+		// TODO 응답 데이터에서 같은 이름의 아이템이 있는 지 확인
+		return false;
+	}
+}
