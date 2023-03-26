0880c3c1842cf4027d39227382b11e241fa1f0c9
this-is-spear <rjsckdd12@gmail.com>
2023-03-26 14:58:47 +0900

    Feat(Storage) 추가 ObjectStorage
    ###Description###
    
    - 추가(ObjectMyBoxStorage) 기능 구현
    - 테스트(ObjectMyBoxStorage) 단위 테스트 진행

---
+++ src/main/java/hello/numblemybox/ObjectStorageProperties.java
@@ -0,0 +1,9 @@
+package hello.numblemybox;
+
+public class ObjectStorageProperties {
+	public final static String ENDPOINT = "https://kr.object.ncloudstorage.com";
+	public final static String REGION = "kr-standard";
+	public final static String ACCESS = "8jOB0VungyBgHzL4n9Xe";
+	public final static String SECRET = "z8ehTkH3RJJLPSWFuA1Jom3idK2DVTtE7SDeC4TO";
+	public final static String BUCKET = "this-is-spear-box";
+}
---
+++ src/test/java/hello/numblemybox/mybox/infra/ObjectMyBoxStorageTest.java
@@ -0,0 +1,38 @@
+package hello.numblemybox.mybox.infra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import hello.numblemybox.stubs.FilePartStub;
+import reactor.core.publisher.Mono;
+
+class ObjectMyBoxStorageTest {
+	ObjectMyBoxStorage objectMyBoxStorage = new ObjectMyBoxStorage();
+
+	@Test
+	@DisplayName("오브젝트 스토리지에 파일 업로드 후 다운로드한다.")
+	void name() throws IOException {
+		var path = Paths.get("./src/test/resources/uplaod/test-text.txt");
+		var fileId = "object-storage-test";
+		objectMyBoxStorage.uploadFile(Mono.just(new FilePartStub(path)), fileId).subscribe();
+
+		var stream = objectMyBoxStorage.downloadFile(Mono.just(fileId)).block();
+		StringBuilder textBuilder = new StringBuilder();
+		try (Reader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+			int c;
+			while ((c = reader.read()) != -1) {
+				textBuilder.append((char)c);
+			}
+		}
+		System.out.println(textBuilder);
+
+		objectMyBoxStorage.deleteFile(fileId);
+	}
+}
